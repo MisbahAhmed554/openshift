88173c249dcd6ee7bb4a37793ee644eb17841e30
MisbahAhmed554 <58257364+MisbahAhmed554@users.noreply.github.com>
2020-09-06 10:50:41 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page for TECHMAHINDRA v2.1</h1>
+
+<?php
+echo "Hello World! from GIT. Tech Mahindra";
+echo "Hello from version1.1   ";
+echo " Why to Worry ? S2I in openshift";
+?>
+
+</body>
+</html>
+
