99df1fcc7ba9fba7705b9e9d82030af8b33783a7
MisbahAhmed554 <58257364+MisbahAhmed554@users.noreply.github.com>
2020-09-06 10:59:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page for TECHMAHINDRA v2.1</h1>
 
 <?php
-echo "Hello World! from GIT. Tech Mahindra";
+echo "Hello World!";
 echo "Hello from version1.1   ";
 echo " Why to Worry ? S2I in openshift";
 ?>
